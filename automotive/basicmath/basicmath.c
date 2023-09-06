dac73b1e9c5affff203b4eb7c67efd2cf3a3c53c
aottaviano <aottaviano@iis.ee.ethz.ch>
2023-09-06 21:24:01 +0200

    [WIP] automotive: Adapt basicmath and bitcount for cheshire on FPGA

--- automotive/basicmath/basicmath.c
+++ automotive/basicmath/basicmath.c
@@ -30,108 +30,108 @@ int main(/*int argc, char** argv*/void)
       long n = 0;
 
       /* solve soem cubic functions */
-      PRINTF("********* CUBIC FUNCTIONS ***********\n");
+      PRINTF("********* CUBIC FUNCTIONS ***********\r\n");
       /* should get 3 solutions: 2, 6 & 2.5   */
       SolveCubic(a1, b1, c1, d1, &solutions, x);  
-      PRINTF("Solutions:");
+      PRINTF("Solutions:\r\n");
       for(i=0;i<solutions;i++)
-        PRINTF(" %f",x[i]);
+        PRINTF(" %f\r\n",x[i]);
       PRINTF("\n");
 
-//      a1 = 1.0; b1 = -4.5; c1 = 17.0; d1 = -30.0;
-//      /* should get 1 solution: 2.5           */
-//      SolveCubic(a1, b1, c1, d1, &solutions, x);  
-//      PRINTF("Solutions:");
-//      for(i=0;i<solutions;i++)
-//        PRINTF(" %f",x[i]);
-//      PRINTF("\n");
-//
-//      a1 = 1.0; b1 = -3.5; c1 = 22.0; d1 = -31.0;
-//      SolveCubic(a1, b1, c1, d1, &solutions, x);
-//      PRINTF("Solutions:");
-//      for(i=0;i<solutions;i++)
-//        PRINTF(" %f",x[i]);
-//      PRINTF("\n");
-//
-//      a1 = 1.0; b1 = -13.7; c1 = 1.0; d1 = -35.0;
-//      SolveCubic(a1, b1, c1, d1, &solutions, x);
-//      PRINTF("Solutions:");
-//      for(i=0;i<solutions;i++)
-//        PRINTF(" %f",x[i]);
-//      PRINTF("\n");
-//
-//      a1 = 3.0; b1 = 12.34; c1 = 5.0; d1 = 12.0;
-//      SolveCubic(a1, b1, c1, d1, &solutions, x);
-//      PRINTF("Solutions:");
-//      for(i=0;i<solutions;i++)
-//        PRINTF(" %f",x[i]);
-//      PRINTF("\n");
-//
-//      a1 = -8.0; b1 = -67.89; c1 = 6.0; d1 = -23.6;
-//      SolveCubic(a1, b1, c1, d1, &solutions, x);
-//      PRINTF("Solutions:");
-//      for(i=0;i<solutions;i++)
-//        PRINTF(" %f",x[i]);
-//      PRINTF("\n");
-//
-//      a1 = 45.0; b1 = 8.67; c1 = 7.5; d1 = 34.0;
-//      SolveCubic(a1, b1, c1, d1, &solutions, x);
-//      PRINTF("Solutions:");
-//      for(i=0;i<solutions;i++)
-//        PRINTF(" %f",x[i]);
-//      PRINTF("\n");
-//
-//      a1 = -12.0; b1 = -1.7; c1 = 5.3; d1 = 16.0;
-//      SolveCubic(a1, b1, c1, d1, &solutions, x);
-//      PRINTF("Solutions:");
-//      for(i=0;i<solutions;i++)
-//        PRINTF(" %f",x[i]);
-//      PRINTF("\n");
-//
-//      /* Now solve some random equations */
-//      for(a1=1;a1<10;a1+=1) {
-//        for(b1=10;b1>0;b1-=.25) {
-//          for(c1=5;c1<15;c1+=0.61) {
-//           for(d1=-1;d1>-5;d1-=.451) {
-//            SolveCubic(a1, b1, c1, d1, &solutions, x);  
-//            PRINTF("Solutions:");
-//            for(i=0;i<solutions;i++)
-//              PRINTF(" %f",x[i]);
-//            PRINTF("\n");
-//           }
-//          }
-//        }
-//      }
+      a1 = 1.0; b1 = -4.5; c1 = 17.0; d1 = -30.0;
+      /* should get 1 solution: 2.5           */
+      SolveCubic(a1, b1, c1, d1, &solutions, x);  
+      PRINTF("Solutions:\r\n");
+      for(i=0;i<solutions;i++)
+        PRINTF(" %f\r\n",x[i]);
+      PRINTF("\n");
+
+      a1 = 1.0; b1 = -3.5; c1 = 22.0; d1 = -31.0;
+      SolveCubic(a1, b1, c1, d1, &solutions, x);
+      PRINTF("Solutions:\r\n");
+      for(i=0;i<solutions;i++)
+        PRINTF(" %f\r\n",x[i]);
+      PRINTF("\n");
+
+      a1 = 1.0; b1 = -13.7; c1 = 1.0; d1 = -35.0;
+      SolveCubic(a1, b1, c1, d1, &solutions, x);
+      PRINTF("Solutions:\r\n");
+      for(i=0;i<solutions;i++)
+        PRINTF(" %f\r\n",x[i]);
+      PRINTF("\n");
+
+      a1 = 3.0; b1 = 12.34; c1 = 5.0; d1 = 12.0;
+      SolveCubic(a1, b1, c1, d1, &solutions, x);
+      PRINTF("Solutions:\r\n");
+      for(i=0;i<solutions;i++)
+        PRINTF(" %f\r\n",x[i]);
+      PRINTF("\n");
+
+      a1 = -8.0; b1 = -67.89; c1 = 6.0; d1 = -23.6;
+      SolveCubic(a1, b1, c1, d1, &solutions, x);
+      PRINTF("Solutions:\r\n");
+      for(i=0;i<solutions;i++)
+        PRINTF(" %f\r\n",x[i]);
+      PRINTF("\n");
+
+      a1 = 45.0; b1 = 8.67; c1 = 7.5; d1 = 34.0;
+      SolveCubic(a1, b1, c1, d1, &solutions, x);
+      PRINTF("Solutions:\r\n");
+      for(i=0;i<solutions;i++)
+        PRINTF(" %f\r\n",x[i]);
+      PRINTF("\n");
+
+      a1 = -12.0; b1 = -1.7; c1 = 5.3; d1 = 16.0;
+      SolveCubic(a1, b1, c1, d1, &solutions, x);
+      PRINTF("Solutions:\r\n");
+      for(i=0;i<solutions;i++)
+        PRINTF(" %f\r\n",x[i]);
+      PRINTF("\n");
+
+      /* Now solve some random equations */
+      for(a1=1;a1<10;a1+=1) {
+        for(b1=10;b1>0;b1-=.25) {
+          for(c1=5;c1<15;c1+=0.61) {
+           for(d1=-1;d1>-5;d1-=.451) {
+            SolveCubic(a1, b1, c1, d1, &solutions, x);  
+            PRINTF("Solutions:\r\n");
+            for(i=0;i<solutions;i++)
+              PRINTF(" %f\r\n",x[i]);
+            PRINTF("\n");
+           }
+          }
+        }
+      }
 
 
-      PRINTF("********* INTEGER SQR ROOTS ***********\n");
+      PRINTF("********* INTEGER SQR ROOTS ***********\r\n");
       /* perform some integer square roots */
       for (i = 0; i < /*100000*/ 1; i+=2)
         {
           usqrt(i, &q);
                 // remainder differs on some machines
          // PRINTF("sqrt(%3d) = %2d, remainder = %2d\n",
-         PRINTF("sqrt(%3d) = %2d\n",
+         PRINTF("sqrt(%3d) = %2d\r\n",
              i, q.sqrt);
         }
-//      PRINTF("\n");
-//      for (l = 0x3fed0169L; l < 0x3fed4169L; l++)
-//        {
-//         usqrt(l, &q);
-//         //PRINTF("\nsqrt(%lX) = %X, remainder = %X\n", l, q.sqrt, q.frac);
-//         PRINTF("sqrt(%lX) = %X\n", l, q.sqrt);
-//        }
+      PRINTF("\n");
+      for (l = 0x3fed0169L; l < 0x3fed4169L; l++)
+        {
+         usqrt(l, &q);
+         //PRINTF("\nsqrt(%lX) = %X, remainder = %X\n", l, q.sqrt, q.frac);
+         PRINTF("sqrt(%lX) = %X\n", l, q.sqrt);
+        }
 
 
-      PRINTF("********* ANGLE CONVERSION ***********\n");
+      PRINTF("********* ANGLE CONVERSION ***********\r\n");
       /* convert some rads to degrees */
     /*   for (X = 0.0; X <= 360.0; X += 1.0) */
       for (X = 0.0; X <= 360.0; X += .001)
-        PRINTF("%3.0f degrees = %.12f radians\n", X, deg2rad(X));
+        PRINTF("%3.0f degrees = %.12f radians\r\n", X, deg2rad(X));
       puts("");
     /*   for (X = 0.0; X <= (2 * PI + 1e-6); X += (PI / 180)) */
       for (X = 0.0; X <= (2 * PI + 1e-6); X += (PI / 5760))
-        PRINTF("%.12f radians = %3.0f degrees\n", X, rad2deg(X));
+        PRINTF("%.12f radians = %3.0f degrees\r\n", X, rad2deg(X));
       
       
     }
--- automotive/bitcount/bitcnts.c
+++ automotive/bitcount/bitcnts.c
@@ -10,15 +10,51 @@
 #include <stdlib.h>
 #include "conio.h"
 #include <limits.h>
-#include <time.h>
+//#include <time.h>
 #include <float.h>
 #include "bitops.h"
+#include <printf.h>
+#include "regs/system_timer.h"
+#include "util.h"
 
 #define FUNCS  7
 
+// Use lfsr isntead of rand
+#define DEFAULT_SEED 0xcaca5a5adeadbeef
+#define FEEDBACK 0x6c0000397f000032
+
+uint64_t *lfsr_byte_feedback;
+
+uint32_t lfsr_iter_bit(uint64_t lfsr) { return (lfsr & 1) ? ((lfsr >> 1) ^ FEEDBACK) : (lfsr >> 1); }
+
+uint32_t lfsr_iter_byte(uint64_t lfsr, uint64_t *lfsr_byte_feedback) {
+    uint32_t l = lfsr;
+    for (int i = 0; i < 8; i++)
+        l = lfsr_iter_bit(l);
+    return l;
+}
+
+uint32_t lfsr_iter_word(uint64_t lfsr, uint64_t *lfsr_byte_feedback) {
+    uint32_t l = lfsr_iter_byte(lfsr, lfsr_byte_feedback);
+    l = lfsr_iter_byte(l, lfsr_byte_feedback);
+    l = lfsr_iter_byte(l, lfsr_byte_feedback);
+    return lfsr_iter_byte(l, lfsr_byte_feedback);
+}
+
+uint64_t lfsr_64bits(uint64_t lfsr, uint64_t *lfsr_byte_feedback) {
+    uint64_t l = lfsr_iter_byte(lfsr, lfsr_byte_feedback);
+    l = lfsr_iter_byte(l, lfsr_byte_feedback);
+    l = lfsr_iter_byte(l, lfsr_byte_feedback);
+    l = lfsr_iter_byte(l, lfsr_byte_feedback);
+    l = lfsr_iter_byte(l, lfsr_byte_feedback);
+    l = lfsr_iter_byte(l, lfsr_byte_feedback);
+    l = lfsr_iter_byte(l, lfsr_byte_feedback);
+    return lfsr_iter_byte(l, lfsr_byte_feedback);
+}
+
 static int CDECL bit_shifter(long int x);
 
-int main(int argc, char *argv[])
+int main(/*int argc, char *argv[]*/)
 {
   clock_t start, stop;
   double ct, cmin = DBL_MAX, cmax = 0;
@@ -45,35 +81,46 @@ int main(int argc, char *argv[])
     "Non-recursive bit count by bytes (AR)",
     "Shift and count bits"
   };
-  if (argc<2) {
-    fprintf(stderr,"Usage: bitcnts <iterations>\n");
-    exit(-1);
-	}
-  iterations=atoi(argv[1]);
+//  if (argc<2) {
+//    PRINTF(stderr,"Usage: bitcnts <iterations>\n");
+//    exit(-1);
+//	}
+  iterations=1000000;/*atoi(argv[1])*/
   
-  puts("Bit counter algorithm benchmark\n");
-  
+  PRINTF("Bit counter algorithm benchmark\r\n");
+
+  seed = DEFAULT_SEED; 
+
+  // We do not use time.h. Use our timer instead, if available
   for (i = 0; i < FUNCS; i++) {
-    start = clock();
+      chs_playgnd_reset_timer();
+      chs_playgnd_start_timer();
+      //start = clock();
     
-    for (j = n = 0, seed = rand(); j < iterations; j++, seed += 13)
+      for (j = n = 0, seed = lfsr_64bits(seed, lfsr_byte_feedback); j < iterations; j++, seed += 13) {
 	 n += pBitCntFunc[i](seed);
+      }
     
-    stop = clock();
-    ct = (stop - start) / (double)CLOCKS_PER_SEC;
-    if (ct < cmin) {
-	 cmin = ct;
-	 cminix = i;
-    }
-    if (ct > cmax) {
-	 cmax = ct;
-	 cmaxix = i;
-    }
-    
-    printf("%-38s> Time: %7.3f sec.; Bits: %ld\n", text[i], ct, n);
+    //stop = clock();
+    chs_playgnd_stop_timer();
+      
+    volatile uint32_t ct_cycles = chs_playgnd_get_timer_count();
+
+    //ct = (stop - start) / (double)CLOCKS_PER_SEC;
+    //if (ct < cmin) {
+    //	 cmin = ct;
+    //	 cminix = i;
+    //}
+    //if (ct > cmax) {
+    //	 cmax = ct;
+    //	 cmaxix = i;
+    //}
+    //
+    //PRINTF("%-38s> Time: %7.3f sec.; Bits: %ld\n", text[i], ct, n);
+    PRINTF("%-38s> Clock cycles: %d; Bits: %ld\r\n", text[i], ct_cycles, n);
   }
-  printf("\nBest  > %s\n", text[cminix]);
-  printf("Worst > %s\n", text[cmaxix]);
+  //PRINTF("\nBest  > %s\n", text[cminix]);
+  //PRINTF("Worst > %s\n", text[cmaxix]);
   return 0;
 }
 
