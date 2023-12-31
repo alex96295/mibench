d2f765c03d57c47d883c2cb90a80c467cf7f8a68
aottaviano <aottaviano@iis.ee.ethz.ch>
2023-12-31 14:45:55 +0100

    automotive: Fix variable override in makefrags

--- automotive/susan/susan.mk
+++ automotive/susan/susan.mk
@@ -1,11 +1,11 @@
 .PHONY: all automotive-susan
 all: automotive-susan
 
-SRCS_C = $(wildcard $(MIBENCH_DIR)/automotive/susan/*.c)
+SUSAN_SRCS_C = $(wildcard $(MIBENCH_DIR)/automotive/susan/*.c)
 
 automotive-susan:
-	$(CC) $(INCLUDES) $(CCFLAGS) $(SRCS_C) -o $(MIBENCH_DIR)/bin/susan.car.l2.elf $(LDFLAGS) $(LDLINK) $(LDLIBS) -lm
-	$(OBJDUMP) -d -S $(MIBENCH_DIR)/bin/susan.car.l2.elf > $(MIBENCH_DIR)/bin/susan.car.l2.dump
+	$(CC) $(INCLUDES) $(CCFLAGS) $(SUSAN_SRCS_C) -o $(MIBENCH_DIR)/bin/susan.$(ELF_PREFIX).elf $(LDFLAGS) $(LDLINK) $(LDLIBS) -lm
+	$(OBJDUMP) -d -S $(MIBENCH_DIR)/bin/susan.$(ELF_PREFIX).elf > $(MIBENCH_DIR)/bin/susan.$(ELF_PREFIX).dump
 
 .PHONY: clean
 clean:
